f092e4d3cfc5c0433847885c335fa2c2fb042be2
HitaloSalazar <nax.indigo@gmail.com>
2025-10-27 18:56:22 -0500

    cambios al menu

--- MOANSO T/1. Administrador.Designer.cs
+++ MOANSO T/1. Administrador.Designer.cs
@@ -46,9 +46,10 @@
             this.btnRegresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnRegresar.Font = new System.Drawing.Font("Segoe UI", 31F);
             this.btnRegresar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(129)))), ((int)(((byte)(57)))));
-            this.btnRegresar.Location = new System.Drawing.Point(1319, 640);
+            this.btnRegresar.Location = new System.Drawing.Point(995, 501);
+            this.btnRegresar.Margin = new System.Windows.Forms.Padding(2);
             this.btnRegresar.Name = "btnRegresar";
-            this.btnRegresar.Size = new System.Drawing.Size(87, 79);
+            this.btnRegresar.Size = new System.Drawing.Size(70, 63);
             this.btnRegresar.TabIndex = 6;
             this.btnRegresar.Text = "→";
             this.btnRegresar.UseVisualStyleBackColor = false;
@@ -62,9 +63,10 @@
             this.btnIniciarSesion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnIniciarSesion.Font = new System.Drawing.Font("Segoe UI", 17F);
             this.btnIniciarSesion.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(129)))), ((int)(((byte)(57)))));
-            this.btnIniciarSesion.Location = new System.Drawing.Point(1095, 224);
+            this.btnIniciarSesion.Location = new System.Drawing.Point(876, 179);
+            this.btnIniciarSesion.Margin = new System.Windows.Forms.Padding(2);
             this.btnIniciarSesion.Name = "btnIniciarSesion";
-            this.btnIniciarSesion.Size = new System.Drawing.Size(236, 64);
+            this.btnIniciarSesion.Size = new System.Drawing.Size(189, 51);
             this.btnIniciarSesion.TabIndex = 7;
             this.btnIniciarSesion.Text = "Registrar";
             this.btnIniciarSesion.UseVisualStyleBackColor = false;
@@ -78,9 +80,10 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Segoe UI", 17F);
             this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(129)))), ((int)(((byte)(57)))));
-            this.button1.Location = new System.Drawing.Point(1095, 323);
+            this.button1.Location = new System.Drawing.Point(876, 258);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(236, 64);
+            this.button1.Size = new System.Drawing.Size(189, 51);
             this.button1.TabIndex = 8;
             this.button1.Text = "Solicitud PDF";
             this.button1.UseVisualStyleBackColor = false;
@@ -94,9 +97,10 @@
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Segoe UI", 17F);
             this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(129)))), ((int)(((byte)(57)))));
-            this.button2.Location = new System.Drawing.Point(1095, 422);
+            this.button2.Location = new System.Drawing.Point(876, 338);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(236, 64);
+            this.button2.Size = new System.Drawing.Size(189, 51);
             this.button2.TabIndex = 9;
             this.button2.Text = "Reevaluar";
             this.button2.UseVisualStyleBackColor = false;
@@ -105,18 +109,20 @@
             // 
             this.groupBox1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(10, 10);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(1013, 695);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(810, 556);
             this.groupBox1.TabIndex = 10;
             this.groupBox1.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(21)))), ((int)(((byte)(22)))));
-            this.ClientSize = new System.Drawing.Size(1407, 719);
+            this.ClientSize = new System.Drawing.Size(1096, 575);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -124,6 +130,7 @@
             this.Controls.Add(this.btnRegresar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
